2e727c5728faeaa5bc063f2c8aa864d96607a09c
Hendra Sucipto <nurhendras.developer@gmail.com>
2021-06-28 11:22:07 +0700

    Final Commit

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
-var nama = "Zero";
-var peran = "Penyihir";
+var nama = "Hendra";
+var peran = "Tabib"; // Ksatria, Tabib, Penyihir
 
 if (nama === "") {
-    console.log("Nama harus diisi")
+  console.log("Nama harus diisi");
 } else if (peran === "") {
-    console.log ("Halo Hendra, Pilih peranmu")
-} else if (nama === "Nina" && peran == "Ksatria") {
-    console.log ("Selamat datang di Dunia Proxytia, Nina. Halo Ksatria Nina, kamu dapat menyerang dengan senjatamu!")
-} else if (nama === "Danu" && peran == "Tabib") {
-    console.log ("Selamat datang di Dunia Proxytia, Danu. Halo Tabib Danu, kamu dapat menyerang dengan senjatamu!")
-} else if (nama !== "" && peran !== "") {
-    console.log ("Selamat datang di Dunia Proxytia, Zero. Halo Penyihir Zero, kamu dapat menyerang dengan senjatamu!")
+  console.log("Halo Hendra, Pilih peranmu");
+} else if (peran == "Ksatria") {
+  console.log(
+    `Selamat datang di Dunia Proxytia, ${nama}. Halo ${peran} ${nama}, kamu dapat menyerang dengan senjatamu!`
+  );
+} else if (peran == "Tabib") {
+  console.log(
+    `Selamat datang di Dunia Proxytia, ${nama}. Halo ${peran} ${nama}, kamu akan membantu temanmu yang terluka`
+  );
+} else if (peran === "Penyihir") {
+  console.log(
+    `Selamat datang di Dunia Proxytia, ${nama}. Halo ${peran} ${nama}, ciptakan keajaiban yang dapat membantu kemenanganmu!`
+  );
+} else {
+  console.log("invalid");
 }
